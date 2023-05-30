794fd327d8a726ffb39a40e52ca99951e2e0f56c
lzaleph <48612942+lzaleph@users.noreply.github.com>
2023-05-30 15:03:38 +0800

    Delete windows directory

--- windows/compile.bat
+++
@@ -1,3 +0,0 @@
-coffee.exe -c -j ..\web\playWeb.js ..\playWeb.coffee ..\basicAI.coffee ..\cards.coffee ..\gameState.coffee
-coffee.exe -c ..\web\multiLog.coffee
-coffee.exe -c ..\web\scoreTracker.coffee
